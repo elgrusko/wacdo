f12c2b33331c9db6bc025840dcb254a8054f4f49
elgrusko <e.grusko@gmail.com>
2026-03-03 10:34:25 +0100

    fix: handle empty password in case of edit collaborator

--- waldo/accounts/forms.py
+++ waldo/accounts/forms.py
@@ -4,6 +4,15 @@ from types_poste.models import TypePoste
 
 User = get_user_model()
 
+'''
+This form : 
+- is used to create and edit collaborators (users)
+- includes a password field that is hashed when saved
+- in edit mode, the password field is optional and if left empty, the current password is
+    kept unchanged
+- includes a search form to filter collaborators by username, first name, last name, email and unassigned status
+- includes a form to filter affectations by position type and start date
+'''
 class CollaboratorCreationForm(forms.ModelForm):
     # PasswordInput widget to render the password field as a password input (with dots instead of characters)
     password = forms.CharField(widget=forms.PasswordInput)
@@ -23,18 +32,24 @@ class CollaboratorCreationForm(forms.ModelForm):
     # In edit mode, password can be left empty to keep the current password.
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # "instance" is the user being edited.
+        # get the initial password hash to keep it if the password field is left empty in edit mode.
+        self._initial_password_hash = self.instance.password if self.instance and self.instance.pk else None
         # Instance with PK means we are in edit mode, so we make the password field not required and add a help text to indicate that leaving it empty will keep the current password.
         if self.instance and self.instance.pk:
             self.fields['password'].required = False
             self.fields['password'].help_text = "Laissez vide pour conserver le mot de passe actuel."
 
-    # override the save method to set the password correctly (hash it) before saving the user to the database
+    # Override save to hash a new password, or keep the existing hash in edit mode.
     def save(self, commit=True):
-        # commit=False to get the user instance without saving it to the database yet, so we can set the password correctly before saving
         user = super().save(commit=False)
         password = self.cleaned_data.get('password')
+
         if password:
             user.set_password(password)
+        elif self._initial_password_hash:
+            user.password = self._initial_password_hash
+
         if commit:
             user.save()
         return user

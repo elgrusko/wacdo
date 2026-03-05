4a310a1d89ed25191fc4688bade30c7aff4a7aaa
elgrusko <e.grusko@gmail.com>
2026-03-05 11:22:17 +0100

    fix build.sh to deploy (static files)

--- waldo/build.sh
+++ waldo/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit # Exit immediately if a command exits with a non-zero status.
+
+# Ensure commands run from this script's directory, whatever Render's current dir is.
+cd "$(dirname "$0")"
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --settings=waldo.settings
